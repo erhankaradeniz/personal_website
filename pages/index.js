deadcc553f1b7cc4c264a4488281b211b8aa37e6
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-04-20 15:04:14 +0200

    finish up website

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,123 @@ const IndexPage = () => {
         description={`Personal website of Erhan Karadeniz where I showcase my project and blog about React.js, Next.js and more..`}
         canonical={`https://www.erhankaradeniz.com`}
       />
-      <div>Erhann</div>
+      <div className="flex flex-row max-w-screen-lg mx-auto mt-48 px-4 md:px-0 mb-20">
+        <div className="hidden md:block w-1/4"></div>
+        <div className="w-full md:w-3/4">
+          <h1 className="text-3xl font-bold">Erhan Karadeniz</h1>
+          <h2 className="text-yellow-500 font-sans uppercase text-sm">
+            Full Stack Engineer - The Netherlands
+          </h2>
+          <h3 className="text-2xl md:text-4xl font-bold leading-tight py-12">
+            Helping to push brands towards industry leadership by developing
+            products and experiences that combine functionality with striking
+            aesthetics.
+          </h3>
+          <p className="mb-8">
+            Choosing the right partner can be a defining decision in the success
+            of a company. With over 10 years of hands on experience and
+            dedication have resulted in practice to develop award-winning,
+            extensible websites. By combining skills as diverse as creative
+            direction and user experience design with interactive designs, I am
+            able to offer a fundamentally different web development service; one
+            that is inherently ergonomic, and more user-efficient.
+          </p>
+          <p className="mb-8"></p>
+          <p className="mb-8">
+            I run an independent web design/development "studio", and as such,
+            my success is dependent upon the success of my clients. I try to
+            form tight relationships with organizations that partner with me and
+            repay their trust with my expertise, consistency and operational
+            flexibility.
+          </p>
+
+          <p>You can find me on the internet:</p>
+          <ul>
+            <li>
+              <span>LinkedIn — </span>
+              <a
+                href="https://www.linkedin.com/in/erhankaradeniz/"
+                target="_blank"
+                rel="noopener"
+                className="text-yellow-500 no-underline hover:underline"
+                title="Erhan Karadeniz on LinkedIn"
+              >
+                in/erhankaradeniz/
+              </a>
+            </li>
+            <li>
+              <span>Twitter — </span>
+              <a
+                href="https://www.twitter.com/erhankaradeniz"
+                target="_blank"
+                rel="noopener"
+                className="text-yellow-500 no-underline hover:underline"
+                title="Erhan Karadeniz on Twitter"
+              >
+                @erhankaradeniz
+              </a>
+            </li>
+            <li>
+              <span>Github — </span>
+              <a
+                href="https://github.com/erhankaradeniz"
+                target="_blank"
+                rel="noopener"
+                className="text-yellow-500 no-underline hover:underline"
+                title="Erhan Karadeniz on Github"
+              >
+                erhankaradeniz
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex flex-row max-w-screen-lg mx-auto px-4 md:px-0 mb-12">
+        <div className="hidden md:block w-1/4">
+          <span className="text-yellow-500 font-sans font-bold text-sm">
+            CLIENT EXPERIENCE
+          </span>
+        </div>
+        <div className="w-full md:w-3/4 flex flex-row">
+          <div className="w-1/3">
+            <ul className="text-sm leading-loose">
+              <li>CZ Zorgzverzekeraar</li>
+              <li>Supertrash</li>
+              <li>Blink Educatie</li>
+              <li>Serkan Aktürk</li>
+              <li>Derya Açıkgöz</li>
+            </ul>
+          </div>
+          <div className="w-1/3">
+            <ul className="text-sm leading-loose">
+              <li>RTL Nederland</li>
+              <li>Ay Yapim</li>
+              <li>GNL Entertainment</li>
+              <li>WRI.io</li>
+              <li>Levent Dörter</li>
+            </ul>
+          </div>
+          <div className="w-1/3">
+            <ul className="text-sm leading-loose">
+              <li>Achmea</li>
+              <li>Aegon</li>
+              <li>DAO Group</li>
+              <li>Hadise</li>
+              <li>Videoland</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row max-w-screen-lg mx-auto px-4 md:px-0">
+        <div className="hidden md:block w-1/4">
+          <span className="text-yellow-500 font-sans font-bold text-sm">
+            BLOG
+          </span>
+        </div>
+        <div className="w-full md:w-3/4 flex flex-row">
+          Blog articles coming soon.. Stay tuned!
+        </div>
+      </div>
     </>
   )
 }
